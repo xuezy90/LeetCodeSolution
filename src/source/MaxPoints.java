2209746970ab8bd25bf0f8eed50c7120567fce9d
xuezy90 <xuezy90@gmail.com>
2014-02-21 17:15:33 +0800

    缺少重复点处理

--- src/source/MaxPoints.java
+++ src/source/MaxPoints.java
@@ -12,9 +12,13 @@ public class MaxPoints {
 	{
 		Point a = null;
 		Point b = null;
-		List<HashSet<String>> setls = null;
-		int maxnumber = 0;
-		Map<Integer,List<HashSet<String>>> map = new HashMap<Integer,List<HashSet<String>>>();
+		List<HashSet<Integer>> setls = null;
+		HashSet<Integer> set = null;
+		boolean newSetFlag = true;
+		int maxnumber = 1;
+		double slope = 0.0;
+		Map<Double,List<HashSet<Integer>>> map = new HashMap<Double,List<HashSet<Integer>>>();
+		if(points == null||points.length == 0) return 0;
 		for(int i = 0;i<points.length-1;i++)
 		{
 			a = points[i];
@@ -23,18 +27,64 @@ public class MaxPoints {
 				b = points[j];
 				if(b.x-a.x == 0) 
 				{
-					if((setls = map.get(Integer.MAX_VALUE)) == null)
-						setls = new ArrayList<HashSet<String>>();
-					for( )
-					
+					if(b.y == a.y) 
+					{
+						
+					}
+					else slope = Double.MAX_VALUE;
 				}
+				else slope = (double)(b.y-a.y)/(double)(b.x-a.x);
+				if(slope == 0.0 || slope == -0.0) slope = 0.0;
+				//System.out.print(a.x+"  "+b.x+"  "+a.y+"  "+b.y+"  "+slope+"  ");
+				if((setls = map.get(slope)) == null)
+					setls = new ArrayList<HashSet<Integer>>();
+				for(Set<Integer> s:setls)
+				{
+					if(s.contains(i)) {
+						s.add(j);
+						newSetFlag = false;
+					}
+					else if(s.contains(i)){
+						s.add(j);
+						newSetFlag = false;
+					}
+					if(s.size()>maxnumber) 
+						maxnumber = s.size();
+					//System.out.println(s);
+				}
+				if(newSetFlag) {
+					set = new HashSet<Integer>();
+					set.add(i);
+					set.add(j);
+					setls.add(set);
+					if(maxnumber<2) maxnumber = 2;
+				}
+				map.put(slope,setls);
 			}
 		}
-		return 0;
+		return maxnumber;
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int pointNumber = 3; 
+		int maxNumber = 0;
+		MaxPoints mp = new MaxPoints();
+		Point[] points = new Point[pointNumber];
+		Point p1 = new Point(2,3);
+		Point p2 = new Point(3,3);
+		Point p3 = new Point(-5,3);
+//		for(int i = 0;i<pointNumber;i++)
+//		{
+//			p = new Point((int)(Math.random()*2),(int)(Math.random()*2));
+//			points[i] = p;
+//		}
 
+		points[0] = p1;
+		points[1] = p2;
+		points[2] = p3;
+		long stime = System.currentTimeMillis();
+		maxNumber = mp.maxPoints(points);
+		long etime = System.currentTimeMillis();
+		System.out.println("共有点 "+pointNumber+" ,直线上最多点数： "+maxNumber+",耗时： "+(etime-stime)+" ms");
 	}
 	
 }
