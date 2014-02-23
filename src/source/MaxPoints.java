1a777189290fa5005214c8258e2f80f9e510c78e
xue <xuezy90@126.com>
2014-02-23 14:47:39 +0800

    coplete MaxPoints

--- src/source/MaxPoints.java
+++ src/source/MaxPoints.java
@@ -1,87 +1,89 @@
 package source;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 public class MaxPoints {
 	
 	public int maxPoints(Point[] points)
 	{
-		Point a = null;
-		Point b = null;
-		List<HashSet<Integer>> setls = null;
-		HashSet<Integer> set = null;
-		boolean newSetFlag = true;
+//		List<HashSet<Integer>> setls = null;
+//		HashSet<Integer> set = null;
+		
+		int kcounter = 0;
+		int duplicate = 0;		
 		int maxnumber = 2;
 		double slope = 0.0;
-		Map<Double,List<HashSet<Integer>>> map = new HashMap<Double,List<HashSet<Integer>>>();
+		
+		Map<Double,Integer> map = new HashMap<Double,Integer>();
 		if(points == null||points.length == 0) return 0;
 		else if(points.length == 1) return 1;
+		
 		for(int i = 0;i<points.length-1;i++)
 		{
-			a = points[i];
+			map.clear();
+			duplicate = 1;
 			for(int j = i+1;j<points.length;j++)
 			{
-				b = points[j];
-				if(b.x-a.x == 0) 
+//				if(i == j) continue;
+				if(points[j].x-points[i].x == 0) 
 				{
-					if(b.y == a.y) 
+					if(points[j].y == points[i].y)
 					{
-						
+						duplicate++;
+						map.put(Double.MIN_VALUE, 0);
+						continue;
 					}
 					else slope = Double.MAX_VALUE;
 				}
-				else slope = (double)(b.y-a.y)/(double)(b.x-a.x);
+				else slope = (double)(points[j].y-points[i].y)/(double)(points[j].x-points[i].x);
 				if(slope == 0.0 || slope == -0.0) slope = 0.0;
-				//System.out.print(a.x+"  "+b.x+"  "+a.y+"  "+b.y+"  "+slope+"  ");
-				if((setls = map.get(slope)) == null)
-					setls = new ArrayList<HashSet<Integer>>();
-				for(Set<Integer> s:setls)
-				{
-					if(s.contains(i)) {
-						s.add(j);
-						newSetFlag = false;
-					}
-					else if(s.contains(i)){
-						s.add(j);
-						newSetFlag = false;
-					}
-					if(s.size()>maxnumber) 
-						maxnumber = s.size();
-					//System.out.println(s);
+				if(map.get(slope) == null) map.put(slope, 1);
+				else{
+					kcounter = map.get(slope);
+					kcounter++;
+					map.put(slope, kcounter);
 				}
-				if(newSetFlag) {
-					set = new HashSet<Integer>();
-					set.add(i);
-					set.add(j);
-					setls.add(set);
-					if(maxnumber<2) maxnumber = 2;
-				}
-				map.put(slope,setls);
+			}
+			for(double d:map.keySet())
+			{
+				if((map.get(d)+duplicate)>maxnumber) maxnumber = map.get(d) + duplicate;
 			}
 		}
 		return maxnumber;
 	}
 	public static void main(String[] args) {
-		final int pointNumber = 3; 
+		final int pointNumber = 10000; 
 		int maxNumber = 0;
 		MaxPoints mp = new MaxPoints();
 		Point[] points = new Point[pointNumber];
-		Point p1 = new Point(2,3);
-		Point p2 = new Point(3,3);
-		Point p3 = new Point(-5,3);
-//		for(int i = 0;i<pointNumber;i++)
-//		{
-//			p = new Point((int)(Math.random()*2),(int)(Math.random()*2));
-//			points[i] = p;
-//		}
+		Point p = null;
+//		Point p1 = new Point(0,0);
+//		Point p2 = new Point(0,0);
+//		Point p3 = new Point(3,4);
+//		Point p4 = new Point(3,4);
+//		Point p5 = new Point(4,4);
+//		Point p6 = new Point(4,4);
+//		Point p7 = new Point(5,5);
+//		Point p8 = new Point(6,6);
+//		Point p9 = new Point(5,5);
+//		Point p10 = new Point(7,8);
+		for(int i = 0;i<pointNumber;i++)
+		{
+			p = new Point((int)(Math.random()*10),(int)(Math.random()*10));
+			points[i] = p;
+		}
 
-		points[0] = p1;
-		points[1] = p2;
-		points[2] = p3;
+//		points[0] = p1;
+//		points[1] = p2;
+//		points[2] = p3;
+//		points[3] = p4;
+//		points[4] = p5;
+//		points[5] = p6;
+//		points[6] = p7;
+//		points[7] = p8;
+//		points[8] = p9;
+//		points[9] = p10;
+
 		long stime = System.currentTimeMillis();
 		maxNumber = mp.maxPoints(points);
 		long etime = System.currentTimeMillis();
